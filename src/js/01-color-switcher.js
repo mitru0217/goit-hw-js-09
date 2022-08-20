4ea341d7f4a6676f04647704607c7f2e3ee1ebb2
Michael <mitru0217@gmail.com>
2022-08-20 13:00:03 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+const startBTn = document.querySelector("button[data-start]");
+const stopBtn = document.querySelector("button[data-stop]");
+       const body = document.querySelector("body");
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
