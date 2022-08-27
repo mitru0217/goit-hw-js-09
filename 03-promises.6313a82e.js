d63ce1b9977e26cc5fb38dbac2f392092625d0a5
mitru0217 <100318106+mitru0217@users.noreply.github.com>
2022-08-27 20:55:50 +0000

    Deploying to gh-pages from @ mitru0217/goit-hw-js-09@2a796621f1ff026ab382df67b84e48d435cad15f 🚀

---
+++ 03-promises.6313a82e.js
@@ -0,0 +1,2 @@
+const o=document.querySelector(".form");console.log(o);const e=document.querySelector("button");console.log(e),o.addEventListener("input",(function(o){formData[o.target.name]=o.target.value,console.log(formData)}));
+//# sourceMappingURL=03-promises.6313a82e.js.map
---
+++ 03-promises.bf26940d.js
@@ -0,0 +1,2 @@
+!function(){var o=document.querySelector(".form");console.log(o);var e=document.querySelector("button");console.log(e),o.addEventListener("input",(function(o){formData[o.target.name]=o.target.value,console.log(formData)}))}();
+//# sourceMappingURL=03-promises.bf26940d.js.map
