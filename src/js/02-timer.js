dafc637507c2745c52f69e4215c2f1c16f35657f
Michael <mitru0217@gmail.com>
2022-08-30 23:07:46 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -53,7 +53,7 @@ class Timer {
       let deadLine = Date.parse(refs.input.value);
       const deltaTime = deadLine -currentTime;
       const ms = this.convertMs(deltaTime);
-      if(deltaTime  <= 0) {
+      if(ms <= 0) {
         clearInterval(this.intervalId);
         return;
       }
